233f2e5ab4ab48d9a49f624d354fa49eddfe962e
ghj <2290804043@qq.com>
2022-09-13 11:48:49 +0800

    hot-fix Commit4

--- src/main/java/com/ghj/test.java
+++ src/main/java/com/ghj/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix111");
         System.out.println("hello hot-fix222");
+        System.out.println("hello hot-fix333");
     }
 }
